30536f1423901a056f9556679ccc8a3287a29421
Berlin <berlinchoose@gmail.com>
2017-08-10 10:53:03 +0800

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,9 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule} from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -28,16 +31,19 @@ import { AppRoutingModule} from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [ DishService, PromotionService, LeaderService ],
+  entryComponents:[ LoginComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
