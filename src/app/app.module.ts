9c20ac17983eb2ee2218a4729f637abe8f824720
Berlin <berlinchoose@gmail.com>
2017-08-14 14:15:50 +0800

    HTTP Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,9 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms'; 
 import { ReactiveFormsModule } from '@angular/forms'; 
 
+import { HttpModule } from '@angular/http';
+import { baseURL } from './shared/baseurl';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,9 +45,11 @@ import { ReactiveFormsModule } from '@angular/forms';
     FlexLayoutModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [ DishService, PromotionService, LeaderService ],
+  providers: [ DishService, PromotionService, LeaderService,
+    { provide: 'BaseURL', useValue: baseURL }, ProcessHTTPMsgService ],
   entryComponents:[ LoginComponent ],
   bootstrap: [AppComponent]
 })
